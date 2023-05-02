1ecb27a99fdde566d64c2cf87f184c3b369001db
thinh-wee <thinh@wee.vn>
2023-05-02 17:13:04 +0700

    add  submitForm register

--- application/login/register/script.js
+++ application/login/register/script.js
@@ -30,3 +30,42 @@ function getInputNewPassword() {
     
     console.log("password=", password.value);
 }
+
+function submitForm() {
+
+    let registerForm = document.getElementById("registerForm");
+    if (Object.is(registerForm, null)) {
+        // throw error
+        console.error("document.getElementById(\"registerForm\") exception!");
+        return
+    };
+
+    registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let username = document.getElementById("inputUsername");
+        let contact = document.getElementById("inputContact");
+        let password = document.getElementById("inputNewPassword");
+
+        if (username.value == "" || contact.value ==  "" || password.value == "") {
+            // throw error
+            console.log("Ensure you input a value in both fields!");
+        } else {
+            // perform operation with form input
+            console.log(
+                `This form has a username of ${username.value} and password of ${password.value}.`,
+                `This orm has a contact of ${contact.value}.`
+            );
+            // action submitted
+            // alert("This form has been successfully submitted!");
+
+            
+            // clean data
+            username.value = "";
+            contact.value = "";
+            password.value = "";
+            // navigate to home page
+            window.location.replace("/login/#");
+        }
+    });
+}
